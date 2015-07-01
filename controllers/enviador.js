86c8345a0df3248e1865a441178e4b3d8677ba48
Carlos Augusto <carlostwmf@gmail.com>
2015-06-30 23:28:51 -0300

    Corrigindo enviador de e-mail

--- controllers/enviador.js
+++ controllers/enviador.js
@@ -33,7 +33,7 @@ module.exports = function() {
 			enviarEmail: function(request, response) {
 				var nome = request.body.nome;
 				var email = request.body.email;
-				enviarEmail(nome, email);
+				controller.enviarEmail(nome, email);
 				response.render("form", { "success" : "Enviado!" });
 			}
 		}
